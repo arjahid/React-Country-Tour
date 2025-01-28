f00d774325f515a179630bb149fd9c88de3870f6
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-29 00:57:32 +0600

    added button

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import "./country.css";
 const Country = ({ country }) => {
   console.log(country);
   const { name, flags, capital, timezones, population } = country;
+  const [visited,setVisited]=useState(false)
+  const handleVidited=()=>{
+    setVisited(!visited)
+  }
   return (
-    <div className="country">
+    <div className={`country ${visited && 'visited'}`}>
       <h3>Name: {name.common}</h3>
       <img src={flags.png} alt="" />
       <p>Capital: {capital}</p>
       <p>Population: {population} </p>
       <p>Time-Zone: {timezones} </p>
+      <button onClick={handleVidited}>{visited?'I have visited':'No i am not visited'}</button>
+      {visited && ' this country'}
+      {visited && ' this country'}
     </div>
   );
 };
